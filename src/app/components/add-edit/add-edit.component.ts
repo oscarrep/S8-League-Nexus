cea1b95e70e5bc227035fc2880a13c3dc0549664
oscarrep <oscar.bcn.95@gmail.com>
2025-03-24 20:15:36 +0100

    add-edit component

--- src/app/components/add-edit/add-edit.component.ts
+++ src/app/components/add-edit/add-edit.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-add-edit',
-  imports: [],
+  imports: [RouterModule, ReactiveFormsModule],
   templateUrl: './add-edit.component.html',
   styleUrl: './add-edit.component.scss'
 })
