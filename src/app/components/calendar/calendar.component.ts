763d756f3cfbe837ad328beb72e1043f78aef54f
oscarrep <oscar.bcn.95@gmail.com>
2025-03-31 13:50:05 +0200

    Install fullCalendar

--- src/app/components/calendar/calendar.component.ts
+++ src/app/components/calendar/calendar.component.ts
@@ -1,13 +1,59 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { FullCalendarModule } from '@fullcalendar/angular';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import timeGridPlugin from '@fullcalendar/timegrid';
+import interactionPlugin from '@fullcalendar/interaction';
+import enLocale from '@fullcalendar/core/locales/en-gb';
 
 @Component({
   selector: 'app-calendar',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, FullCalendarModule],
   templateUrl: './calendar.component.html',
   styleUrl: './calendar.component.scss'
 })
-export class CalendarComponent {
+export class CalendarComponent implements OnInit {
+  calendarOptions: any;
+  calendarEvents!: any[];
+
+
+
+  ngOnInit(): void {
+
+    this.calendarOptions = {
+      plugins: [dayGridPlugin, timeGridPlugin, interactionPlugin],
+      defaultDate: new Date(),
+      header: {
+        left: 'prev, next',
+        center: 'title',
+        right: 'dayGridMonth,timeGridWeek,timeGridDay',
+      },
+      locale:enLocale,
+      editable: false
+    }
+
+    this.calendarEvents = [
+      {
+        title: 'Event',
+        start: new Date(),
+        description: 'qwe'
+      },
+      {
+        title: 'Event2',
+        start: new Date(),
+        description: 'asdf'
+      },
+      {
+        title: 'Event3',
+        start: new Date(),
+        description: 'zxc'
+      }
+    ]
+
+
+  }
+
+
 
 }
