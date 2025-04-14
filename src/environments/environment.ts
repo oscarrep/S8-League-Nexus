9cc83f6f7d8e50621a15e4c5482a646e8a25bb12
oscarrep <oscar.bcn.95@gmail.com>
2025-04-14 04:40:19 +0200

    Add environment configuration file

---
+++ src/environments/environment.ts
@@ -0,0 +1,24 @@
+export const environment = {
+    production: false,
+    host: '',
+    apiPlayers:'',
+    
+    geoApi:'',
+    geoKey:'',
+    geoUrl:'',
+    mapUrl:'',
+    
+    apiGames:'',
+    idLEC:'',
+    idLCK:'',
+    idLPL:'',
+    idLCP:'',
+    idNorth:'',
+    idSouth:'',
+    idWorlds:'',
+    idMSI:'',
+
+    esportsUrl:'',
+    esportsEn:'',
+    esportsEsp:'',
+}
